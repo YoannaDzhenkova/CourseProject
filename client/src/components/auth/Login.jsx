a9235d002a0580548cdd376d4c2f0a10221a9d2c
YoannaDzhenkova <yoannadjenkova@gmail.com>
2025-03-31 18:23:38 +0300

    update login.jsx
    updating login.jsx to save the user and show it when creating a review

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,13 +1,22 @@
-import { Link } from 'react-router'
+import { Link, useNavigate } from 'react-router'
 
-export default function Login() {
+export default function Login({
+    onLogin,
+}) {
+    const navigate = useNavigate();
+    const loginAction = (formData) => {
+        const email = formData.get('email');
 
+        onLogin(email);
+        navigate('/services')
+        
+    }
 
     return (
         <>
             <div className="login">
                 <section id="login-page" className="auth">
-                    <form id="login">
+                    <form id="login" action={loginAction}>
                     <img className="brand-logo" src='images/images (1).png' />
                         <div className="container">
                             
